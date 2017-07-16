60837772d68ad10d9bede98a14f434cd6f1963bd
NetBlocks Team <team@netblocks.org>
2017-07-16 08:16:58 +0200

    Persist db

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,16 @@
+const NeDB = require('nedb');
+const path = require('path');
+const service = require('feathers-nedb');
+
+function userModel() {
+  return new NeDB({
+    filename: path.join('examples', 'step', 'data', 'users.db'),
+    autoload: true
+  });
+}
+
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+
+  this.use('/report', service({ Model: userModel() }));
 };
